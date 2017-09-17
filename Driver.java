b24be93ee1478a8f8d7dd89f7b69a9b32d1ebcf6
JayantSravan <jayant.sravan@gmail.com>
2017-09-17 18:33:21 +0530

    Driver added

---
+++ Driver.java
@@ -0,0 +1,20 @@
+public class Driver
+{
+	public static void main(String args[])
+	{
+		Type one = new Type();
+		Type two = new Type();
+		Type three = new Type();
+		Type four = new Type();
+		Type five = new Type();
+		Type six =  new Type();
+		Type seven = new Type();
+		//while(true)
+		//{
+			System.out.println("What to do?");
+			System.out.println("1. Add Train Data");
+			System.out.println("2. Test Sample");
+			System.out.println("3. Print Version Spaces");
+		//}
+	}	
+}
